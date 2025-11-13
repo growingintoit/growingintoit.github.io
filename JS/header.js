8060d67ed8922bbe4631be0e3b5698815dd15f86
Juan Ignacio Szapiro <juanignacioszapiro@gmail.com>
2025-11-13 20:24:12 -0300

    agregado de about us, links

--- JS/header.js
+++ JS/header.js
@@ -1,4 +1,4 @@
 export function isertar_header() {
   document.getElementById("cabecera").innerHTML =
-    '<a href=""><h1>Growing Into It</h1></a><a href="">About us</a>';
+    '<a href="https://growingintoit.github.io/"><h1>Growing Into It</h1></a><a href="https://growingintoit.github.io/about_us">About us</a>';
 }
